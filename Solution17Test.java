b0369046354f938f02892a56860df85407fecc0e
ZJF39 <739376639@qq.com>
2024-12-01 14:10:21 +0800

    添加注释

--- Solution17Test.java
+++ Solution17Test.java
@@ -1,5 +1,3 @@
-
-
 import org.junit.Test;
 
 import java.util.List;
@@ -10,7 +8,7 @@ public class Solution17Test {
 
     // 测试用例1: 检查具有重复DNA序列的字符串
     @Test
-    public void testFindRepeatedDnaSequences_Example1() {
+    public void testFRDS_example1() {
         Solution17 solution = new Solution17();
         String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
         List<String> expected = List.of("AAAAACCCCC", "CCCCCAAAAA");
@@ -20,7 +18,7 @@ public class Solution17Test {
 
     // 测试用例2: 检查由相同字符组成的字符串
     @Test
-    public void testFindRepeatedDnaSequences_Example2() {
+    public void testFRDS_example2() {
         Solution17 solution = new Solution17();
         String s = "AAAAAAAAAAAAA";
         List<String> expected = List.of("AAAAAAAAAA");
@@ -30,7 +28,7 @@ public class Solution17Test {
 
     // 测试用例3: 检查没有重复序列的字符串
     @Test
-    public void testFindRepeatedDnaSequences_NoRepeats() {
+    public void testFRDS_noRepeats() {
         Solution17 solution = new Solution17();
         String s = "ACGTACGTAC";
         List<String> expected = List.of();
@@ -40,7 +38,7 @@ public class Solution17Test {
 
     // 测试用例4: 检查短字符串
     @Test
-    public void testFindRepeatedDnaSequences_ShortString() {
+    public void testFRDS_shortString() {
         Solution17 solution = new Solution17();
         String s = "ACG";
         List<String> expected = List.of();
@@ -50,7 +48,7 @@ public class Solution17Test {
 
     // 测试用例5: 检查空字符串
     @Test
-    public void testFindRepeatedDnaSequences_EmptyString() {
+    public void testFRDS_emptyString() {
         Solution17 solution = new Solution17();
         String s = "";
         List<String> expected = List.of();
